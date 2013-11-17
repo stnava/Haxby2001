d5b69a836e4615ad5b060597ab703a95b2b7295a
brian avants <stnava@gmail.com>
2013-11-17 08:26:54 -0500

    ENH: good results ....
    \begin{table}[ht]
    \centering
    \begin{tabular}{rrrrrrrrrrrrr}
      \hline
     & 1 & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10 & 11 & 12 \\
      \hline
    1 & 100.00 & 100.00 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 \\
      2 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 100.00 & 100.00 \\
      3 & 100.00 & 100.00 & 87.50 & 87.50 & 100.00 & 87.50 & 75.00 & 100.00 & 100.00 & 87.50 & 87.50 & 87.50 \\
      4 & 75.00 & 100.00 & 100.00 & 100.00 & 75.00 & 100.00 & 87.50 & 100.00 & 87.50 & 87.50 & 87.50 & 100.00 \\
      5 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 100.00 \\
      6 & 100.00 & 87.50 & 87.50 & 100.00 & 100.00 & 100.00 & 100.00 & 87.50 & 100.00 & 100.00 & 100.00 & 87.50 \\
       \hline
    \end{tabular}
    \end{table}
    
    allresults file

--- src/haxby_2001_ROI.R
+++ src/haxby_2001_ROI.R
@@ -14,13 +14,13 @@ library(e1071)
 fmriPredictorMatrix <- function( fmri, mask , motionin , selector = NA , ncompcor= 3 , nsvdcomp = 4 )
 {
   mat<-timeseries2matrix( fmri, mask )
-  if ( is.na( selector ) ) selector<-rep(TRUE, 1:nrow( motionin ) )
+  if ( is.na( selector ) ) selector<-rep(TRUE, nrow( motionin ) )
   mat<-mat[ selector, ]
   msvd <- svd(t(motionin[selector,3:ncol(motionin) ]))
   motion <-as.matrix(msvd$v[, 1:nsvdcomp])
-  cmpc<-as.matrix( compcor( mat, ncompcor = ncompcor, fastsvd=FALSE ) )
+  cmpc<-as.matrix( compcor( mat, ncompcor = ncompcor, fastsvd=TRUE ) )
   globsig<-apply( mat , FUN=mean , MARGIN=1 )
-  if ( length(c(cmpc)) == nrow(mat) ) mat<-residuals( lm( mat ~ as.matrix(motion)   ) ) else mat<-residuals( lm( mat ~ as.matrix(motion)  ) )
+  if ( length(c(cmpc)) == nrow(mat) ) mat<-residuals( lm( mat ~ as.matrix(motion) ) ) else mat<-residuals( lm( mat ~ as.matrix(motion)  ) )
   mydot<-F
   if ( mydot ) mat<-temporalwhiten( mat )
   return( list( mat = mat, cmpc=cmpc, globsig=globsig )  )
@@ -64,64 +64,58 @@ if ( ! file.exists("AFFINE.nii.gz") )
 fmri<-antsImageRead("AFFINE.nii.gz",4)
 fmriavg<-antsImageRead("AFFINE_avg.nii.gz",3)
 motionin<-read.csv('AFFINEMOCOparams.csv')
-maskFull<-getMask(fmriavg,100,1.e9,TRUE)
 ############ other masking strategies ###########
-maskbrain<-antsImageRead("fullMask.nii.gz",3)
-maskFull<-antsImageClone(maskbrain)
-ImageMath(3,maskFull,"MD",maskFull,2)
-ImageMath(3,maskFull,"-",maskFull,maskbrain)
-# maskFull<-getMask(fmriavg,250,1.e9,TRUE)
-# ImageMath(3,maskFull,"ME",maskFull,1)
-# ImageMath(3,maskFull,"GetLargestComponent",maskFull)
-antsImageWrite(maskFull , "../temp.nii.gz") 
+maskNuis<-antsImageClone(fmriavg)
+ThresholdImage(3,fmriavg,maskNuis,"Otsu",3)
+ThresholdImage(3,maskNuis,maskNuis,1,1)
 mask<-antsImageRead( 'mask.nii.gz', 3 )
 if ( file.exists("AALlabel.nii.gz") & FALSE  )
   {
     aalimg<-antsImageRead("AALlabel.nii.gz",3)
     aalnums<-c( 55, 56, 89:100, 113 )
-#    aalnums<-sort( unique( aalimg[ mask > 0 & aalimg > 0 ] ) )
+#   aalnums<-sort( unique( aalimg[ mask > 0 & aalimg > 0 ] ) )
     aalimg<-maskImage( aalimg,aalimg, as.list( aalnums ) , binarize=TRUE )
-    aalvoxselection <- ( aalimg <= 100 & aalimg > 0 & maskFull > 0 )
+    aalvoxselection <- ( aalimg <= 100 & aalimg > 0  )
     mask<-antsImageClone( aalimg )
     mask[ !aalvoxselection ]<-0
   }
 ############ other masking strategies end ###########
+library(irlba)
+ncc <- 4
+segmat<-timeseries2matrix( fmri, maskNuis )
+segsvd<-irlba( segmat[,sample(1:ncol(segmat))[1:(ncol(segmat)/4)]] , nu=ncc )
+fmripreds<-fmriPredictorMatrix( fmri, mask, motionin, selector=NA, ncompcor = ncc )
+fullmat<-residuals( lm( fmripreds$mat ~ segsvd$u ) )
+ff<-svd( fullmat )
 for ( wrun in runstotest )
 {
   mask<-antsImageRead('mask.nii.gz',3)
   selector<-( as.numeric( design$chunks ) != wrun   )
   selector2<-( as.numeric( design$chunks ) == wrun  )
   subdesign<-subset( design, selector )
-  ncc <- 4
-  fmripreds<-fmriPredictorMatrix( fmri, mask, motionin, selector, ncompcor = ncc )
-  fmripredsFull<-fmriPredictorMatrix( fmri, maskFull, motionin, selector, ncompcor = ncc )
-  mat<-residuals( lm( fmripreds$mat ~ fmripredsFull$cmpc ) )
-  myclasses <- levels( subdesign$labels )
-#######################
-##### train phase ######
-#######################
-  for ( nv in seq( from=100,to=100,by=5) )
+  for ( nv in seq( from=175,to=175,by=5) )
     {
-    ff<-svd( mat )
-    mysccanimages<-t(ff$v[,1:nv])
-    mysccanpreds <- ( mat  ) %*% t( mysccanimages )
-    mydf         <- data.frame( factpreds = as.factor((subdesign$labels))  , imgs = mysccanpreds )
-    my.rf        <- svm( factpreds ~ . , data=mydf, probability = TRUE ) # , kernel = "linear" ) # , cross = 0 )
-#######################
+##### train phase ######
+    mysccanimages<- (ff$u[selector,1:nv])
+    whichtostudy<-( subdesign$labels != "rest" )
+    mydf         <- data.frame( factpreds = as.factor((subdesign$labels))  , imgs = mysccanimages )
+    mydf <- subset( mydf , whichtostudy )
+    my.rf        <- svm( factpreds ~ . , data=mydf ) # , cross = 0 )
+#    tempfact <-as.factor( rank( as.numeric( mydf$factpreds ) ) )
+#    levels( tempfact )<-levels( mydf$factpreds )[ levels(mydf$factpreds ) != "rest" ]
+#    mydf$factpreds <- tempfact
+#    my.rf        <- randomForest( factpreds ~ . , data=mydf ) 
 ##### test phase ######
-#######################
-  fmriTest     <- fmriPredictorMatrix( fmri, mask,     motionin, selector2, ncompcor = ncc )
-  fmriTestFull <- fmriPredictorMatrix( fmri, maskFull, motionin, selector2, ncompcor = ncc )
-  mat2<-residuals( lm( fmriTest$mat ~ fmriTestFull$cmpc ) )
-  mysccanpreds2 <- ( mat2  ) %*% t( mysccanimages )
-  mydf2<-data.frame(  imgs = mysccanpreds2 ) # , corrs = cor( t( mysccanpreds2 ) ) )
-  mypred2<-predict( my.rf , newdata = mydf2 )
-  subdesign2<-subset( design, selector2 )
-  sublabels<-as.factor((subdesign2$labels))
-  zz<-majoritylabel( sublabels , mypred2 )
-  myrate<-100*(sum(zz$groundtruth==zz$voted)/length(zz$groundtruth))
-  print(paste("CorrectClassify:",myrate,"%",getwd(),wrun+1,' npreds ',nv) )
-  }
+    whichtostudy2<-( subdesign2$labels != "rest" )
+    mydf2<-data.frame(  imgs = ff$u[selector2,1:nv] ) 
+    mydf2 <- subset( mydf2 , whichtostudy2 )
+    mypred2<-predict( my.rf , newdata = mydf2 )
+    subdesign2<-subset( design, selector2 )
+    sublabels<-as.factor((subdesign2$labels))
+    zz<-majoritylabel( sublabels[whichtostudy2] , mypred2 )
+    myrate<-100*(sum(zz$groundtruth==zz$voted)/length(zz$groundtruth))
+    print(paste("CorrectClassify:",myrate,"%",getwd(),wrun+1,' npreds ',nv) )
+    }
   if ( wrun == 0 ) predictedlabels<-zz else predictedlabels<-rbind( predictedlabels, zz )
   myrates[ wrun+1 ]<-myrate
 } # wrun loop
